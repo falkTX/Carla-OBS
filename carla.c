e17d300efd42f6dec8d83da954bc9878023a73a5
falkTX <falktx@falktx.com>
2023-04-10 21:00:57 +0200

    Get some null processing going
    Signed-off-by: falkTX <falktx@falktx.com>

--- carla.c
+++ carla.c
@@ -5,6 +5,7 @@
  */
 
 #include <obs-module.h>
+#include <util/platform.h>
 
 #include "CarlaNativePlugin.h"
 
@@ -40,12 +41,13 @@ struct carla_data {
     size_t channels;
     uint32_t bufferSize;
     uint32_t sampleRate;
+    float* dummyBuffer;
 };
 
 static const char *carla_obs_get_name(void *unused)
 {
     UNUSED_PARAMETER(unused);
-    return obs_module_text("3BandEq");
+    return obs_module_text("Carla Plugin Host");
 }
 
 static void *carla_obs_create(obs_data_t *settings, obs_source_t *filter)
@@ -58,39 +60,57 @@ static void *carla_obs_create(obs_data_t *settings, obs_source_t *filter)
     if (carla == NULL)
         return NULL;
 
+    carla->dummyBuffer = calloc(sizeof(float), 512);
+    if (carla->dummyBuffer == NULL)
+    {
+        free(carla);
+        return NULL;
+    }
+
     const audio_t *audio = obs_get_audio();
     carla->channels = audio_output_get_channels(audio);
     carla->bufferSize = 512;
     carla->sampleRate = audio_output_get_sample_rate(audio);
 
-    NativeHostDescriptor host = {
-        .handle = carla,
-        .resourceDir = carla_get_library_folder(),
-        .uiName = "OBS",
-        .get_buffer_size = host_get_buffer_size,
-        .get_sample_rate = host_get_sample_rate,
-        .is_offline = host_is_offline,
-        .get_time_info = host_get_time_info,
-        .write_midi_event = host_write_midi_event,
-        .ui_parameter_changed = host_ui_parameter_changed,
-        .ui_midi_program_changed = host_ui_midi_program_changed,
-        .ui_custom_data_changed = host_ui_custom_data_changed,
-        .ui_closed = host_ui_closed,
-        .ui_open_file = host_ui_open_file,
-        .ui_save_file = host_ui_save_file,
-        .dispatcher = host_dispatcher
-    };
-
     carla->descriptor = descriptor;
-    carla->host = host;
 
-    carla->handle = descriptor->instantiate(&host);
+    {
+        NativeHostDescriptor host = {
+            .handle = carla,
+            .resourceDir = carla_get_library_folder(),
+            .uiName = "OBS",
+            .get_buffer_size = host_get_buffer_size,
+            .get_sample_rate = host_get_sample_rate,
+            .is_offline = host_is_offline,
+            .get_time_info = host_get_time_info,
+            .write_midi_event = host_write_midi_event,
+            .ui_parameter_changed = host_ui_parameter_changed,
+            .ui_midi_program_changed = host_ui_midi_program_changed,
+            .ui_custom_data_changed = host_ui_custom_data_changed,
+            .ui_closed = host_ui_closed,
+            .ui_open_file = host_ui_open_file,
+            .ui_save_file = host_ui_save_file,
+            .dispatcher = host_dispatcher
+        };
+        carla->host = host;
+    }
+
+    {
+        NativeTimeInfo timeInfo = {
+            .usecs = os_gettime_ns() * 1000,
+        };
+        carla->timeInfo = timeInfo;
+    }
+
+    carla->handle = descriptor->instantiate(&carla->host);
     if (carla->handle == NULL)
         goto fail;
 
     carla->internalHostHandle = carla_create_native_plugin_host_handle(descriptor, carla->handle);
     if (carla->internalHostHandle == NULL)
         goto fail;
+
+    descriptor->activate(carla->handle);
 
     return carla;
 
@@ -106,7 +126,9 @@ static void carla_obs_destroy(void *data)
 {
     struct carla_data *carla = data;
     carla_host_handle_free(carla->internalHostHandle);
+    carla->descriptor->deactivate(carla->handle);
     carla->descriptor->cleanup(carla->handle);
+    free(carla->dummyBuffer);
     bfree(carla);
 }
 
@@ -116,6 +138,19 @@ static void carla_obs_update(void *data, obs_data_t *settings)
 
 static struct obs_audio_data *carla_obs_filter_audio(void *data, struct obs_audio_data *audio)
 {
+    struct carla_data *carla = data;
+    const uint32_t frames = audio->frames;
+
+    float* abuffers[MAX_AV_PLANES];
+    {
+        size_t c = 0;
+        for (; c < carla->channels; c++)
+            abuffers[c] = (float *)audio->data[c];
+        for (; c < 2; c++)
+            abuffers[c] = carla->dummyBuffer;
+    }
+
+    carla->descriptor->process(carla->handle, abuffers, abuffers, frames, NULL, 0);
     return audio;
 }
 
@@ -226,7 +261,7 @@ bool obs_module_load(void)
     filter.destroy = carla_obs_destroy;
     filter.update = carla_obs_update;
     filter.filter_audio = carla_obs_filter_audio;
-    filter.get_defaults = carla_obs_get_defaults,
+    // filter.get_defaults = carla_obs_get_defaults,
     filter.get_properties = carla_obs_get_properties;
     filter.save = carla_obs_save;
 
