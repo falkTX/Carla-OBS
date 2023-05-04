e99a3a6ebb7adfa13b412fcbed6be59cc859bf10
falkTX <falktx@falktx.com>
2023-05-04 19:20:55 +0200

    Test with a single output binary
    Signed-off-by: falkTX <falktx@falktx.com>

--- cmake/legacy.cmake
+++ cmake/legacy.cmake
@@ -8,7 +8,7 @@ set_target_properties(
              FOLDER plugins
              PREFIX "")
 
-set_target_properties(carla-patchbay PROPERTIES FOLDER plugins PREFIX "")
+# set_target_properties(carla-patchbay PROPERTIES FOLDER plugins PREFIX "")
 
 if(_QT_VERSION EQUAL 6 AND OS_WINDOWS)
   set_target_properties(carla-bridge PROPERTIES AUTORCC_OPTIONS "--format-version;1")
@@ -17,6 +17,6 @@ endif()
 # ######################################################################################################################
 
 setup_plugin_target(carla-bridge)
-setup_plugin_target(carla-patchbay)
+# setup_plugin_target(carla-patchbay)
 
 # ######################################################################################################################
