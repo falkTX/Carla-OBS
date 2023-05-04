9e54b785f7dc166a9279b04043762bffe79b5b97
falkTX <falktx@falktx.com>
2023-05-04 22:27:07 +0200

    do like others

--- cmake/legacy.cmake
+++ cmake/legacy.cmake
@@ -1,3 +1,16 @@
+project(carla)
+
+add_library(carla-patchbay MODULE)
+add_library(OBS::carla-patchbay ALIAS carla-patchbay)
+
+target_link_libraries(carla-patchbay PRIVATE OBS::libobs)
+
+target_sources(carla-patchbay PRIVATE carla.c)
+
+set_target_properties(carla-patchbay PROPERTIES FOLDER "plugins/carla")
+
+setup_plugin_target(carla-patchbay)
+
 # ######################################################################################################################
 
 # set_target_properties( carla-bridge PROPERTIES AUTOMOC ON AUTOUIC ON AUTORCC ON FOLDER plugins PREFIX "")
