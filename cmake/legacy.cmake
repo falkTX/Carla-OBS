6ff3086050aae1bd77092e788794aaaf47d93e0f
falkTX <falktx@falktx.com>
2023-05-04 20:16:39 +0200

    no plugin built at all
    Signed-off-by: falkTX <falktx@falktx.com>

--- cmake/legacy.cmake
+++ cmake/legacy.cmake
@@ -1,22 +1,14 @@
 # ######################################################################################################################
 
-set_target_properties(
-  carla-bridge
-  PROPERTIES AUTOMOC ON
-             AUTOUIC ON
-             AUTORCC ON
-             FOLDER plugins
-             PREFIX "")
+# set_target_properties( carla-bridge PROPERTIES AUTOMOC ON AUTOUIC ON AUTORCC ON FOLDER plugins PREFIX "")
 
 # set_target_properties(carla-patchbay PROPERTIES FOLDER plugins PREFIX "")
 
-if(_QT_VERSION EQUAL 6 AND OS_WINDOWS)
-  set_target_properties(carla-bridge PROPERTIES AUTORCC_OPTIONS "--format-version;1")
-endif()
+# if(_QT_VERSION EQUAL 6 AND OS_WINDOWS) set_target_properties(carla-bridge PROPERTIES AUTORCC_OPTIONS
+# "--format-version;1") endif()
 
 # ######################################################################################################################
 
-setup_plugin_target(carla-bridge)
-# setup_plugin_target(carla-patchbay)
+# setup_plugin_target(carla-bridge) setup_plugin_target(carla-patchbay)
 
 # ######################################################################################################################
