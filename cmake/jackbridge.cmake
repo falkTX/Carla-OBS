b20c9e62708d5a01e2c9842c5feaddb12b211fd7
falkTX <falktx@falktx.com>
2023-05-05 23:49:23 +0200

    comp

--- cmake/jackbridge.cmake
+++ cmake/jackbridge.cmake
@@ -15,13 +15,13 @@ endif()
 add_library(carla-jackbridge STATIC)
 mark_as_advanced(carla-jackbridge)
 
-# if(NOT OS_WINDOWS)
-# set_property(TARGET carla-jackbridge PROPERTY POSITION_INDEPENDENT_CODE ON)
-# endif()
+if(NOT OS_WINDOWS)
+  set_property(TARGET carla-jackbridge PROPERTY POSITION_INDEPENDENT_CODE ON)
+endif()
 
-# target_compile_definitions(carla-jackbridge PRIVATE REAL_BUILD)
+target_compile_definitions(carla-jackbridge PRIVATE REAL_BUILD)
 
-# target_include_directories(carla-jackbridge PRIVATE carla/source/includes carla/source/utils)
+target_include_directories(carla-jackbridge PRIVATE carla/source/includes carla/source/utils)
 
 # target_link_libraries(carla-jackbridge PRIVATE ${CMAKE_THREAD_LIBS_INIT} ${carla_jackbridge_extra_libs})
 
