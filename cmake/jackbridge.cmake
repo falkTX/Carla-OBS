6628a6ae2725452eca2d0042dc9cf47d44e6d943
falkTX <falktx@falktx.com>
2023-05-05 23:35:30 +0200

    revert

--- cmake/jackbridge.cmake
+++ cmake/jackbridge.cmake
@@ -15,15 +15,19 @@ endif()
 add_library(carla-jackbridge STATIC)
 mark_as_advanced(carla-jackbridge)
 
-if(NOT OS_WINDOWS)
-  set_property(TARGET carla-jackbridge PROPERTY POSITION_INDEPENDENT_CODE ON)
-endif()
+# if(NOT OS_WINDOWS)
+# set_property(TARGET carla-jackbridge PROPERTY POSITION_INDEPENDENT_CODE ON)
+# endif()
 
-target_compile_definitions(carla-jackbridge PRIVATE REAL_BUILD)
+# target_compile_definitions(carla-jackbridge PRIVATE REAL_BUILD)
 
-target_include_directories(carla-jackbridge PRIVATE carla/source/includes carla/source/utils)
+# target_include_directories(carla-jackbridge PRIVATE carla/source/includes carla/source/utils)
 
-target_link_libraries(carla-jackbridge PRIVATE ${CMAKE_THREAD_LIBS_INIT} ${carla_jackbridge_extra_libs})
+# target_link_libraries(carla-jackbridge PRIVATE ${CMAKE_THREAD_LIBS_INIT} ${carla_jackbridge_extra_libs})
 
-target_sources(carla-jackbridge PRIVATE ${carla_jackbridge_basedir}/JackBridge1.cpp
-                                        ${carla_jackbridge_basedir}/JackBridge2.cpp)
+# target_sources(carla-jackbridge PRIVATE ${carla_jackbridge_basedir}/JackBridge1.cpp
+# ${carla_jackbridge_basedir}/JackBridge2.cpp)
+
+target_link_libraries(carla-jackbridge PRIVATE OBS::libobs)
+
+target_sources(carla-jackbridge PRIVATE common.c)
