20b5fc34822d0b13a147280a6720c2ac027a1ceb
falkTX <falktx@falktx.com>
2023-05-05 23:15:41 +0200

    try the full thing

--- cmake/rtmempool.cmake
+++ cmake/rtmempool.cmake
@@ -18,15 +18,12 @@ if(NOT OS_WINDOWS)
   set_property(TARGET carla_rtmempool PROPERTY POSITION_INDEPENDENT_CODE ON)
 endif()
 
-# target_compile_definitions(carla_rtmempool PRIVATE REAL_BUILD)
+target_compile_definitions(carla_rtmempool PRIVATE REAL_BUILD)
 
-# target_include_directories(carla_rtmempool PRIVATE carla/source/includes carla/source/utils)
+target_include_directories(carla_rtmempool PRIVATE carla/source/includes carla/source/utils)
 
-# target_link_libraries(carla_rtmempool PRIVATE ${CMAKE_THREAD_LIBS_INIT} ${carla_rtmempool_extra_libs})
-target_link_libraries(carla_rtmempool PRIVATE OBS::libobs)
+target_link_libraries(carla_rtmempool PRIVATE ${CMAKE_THREAD_LIBS_INIT} ${carla_rtmempool_extra_libs})
 
-target_sources(carla_rtmempool PRIVATE common.c)
-
-# target_sources(carla_rtmempool PRIVATE ${carla_rtmempool_basedir}/rtmempool.c)
+target_sources(carla_rtmempool PRIVATE ${carla_rtmempool_basedir}/rtmempool.c)
 
 mark_as_advanced(carla_rtmempool)
