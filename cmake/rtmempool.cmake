b213b385b30990bf7dd98b9315565d59765f7668
falkTX <falktx@falktx.com>
2023-05-06 16:49:58 +0200

    fb
    Signed-off-by: falkTX <falktx@falktx.com>

--- cmake/rtmempool.cmake
+++ cmake/rtmempool.cmake
@@ -20,6 +20,8 @@ if(NOT OS_WINDOWS)
   set_property(TARGET carla-rtmempool PROPERTY POSITION_INDEPENDENT_CODE ON)
 endif()
 
+target_include_directories(carla-rtmempool PRIVATE carla/source/includes)
+
 target_link_libraries(carla-rtmempool PRIVATE ${carla_rtmempool_extra_libs})
 
 target_sources(carla-rtmempool PRIVATE ${carla_rtmempool_basedir}/rtmempool.c)
