7fc16408ae69f452edb7d6abe348a8d630a8570a
falkTX <falktx@falktx.com>
2023-05-07 14:42:14 +0200

    pat
    Signed-off-by: falkTX <falktx@falktx.com>

--- cmake/legacy.cmake
+++ cmake/legacy.cmake
@@ -41,7 +41,8 @@ add_library(carla::lilv ALIAS carla-lilv)
 include(cmake/rtmempool.cmake)
 add_library(carla::rtmempool ALIAS carla-rtmempool)
 
-# include(cmake/water.cmake) add_library(carla::water ALIAS carla-water)
+include(cmake/water.cmake)
+add_library(carla::water ALIAS carla-water)
 
 # Setup carla-bridge target
 add_library(carla-bridge MODULE)
@@ -114,33 +115,74 @@ endif()
 
 setup_plugin_target(carla-bridge)
 
-# Setup carla-patchbay target add_library(carla-patchbay MODULE) add_library(OBS::carla-patchbay ALIAS carla-patchbay)
+# Setup carla-patchbay target
+add_library(carla-patchbay MODULE)
+add_library(OBS::carla-patchbay ALIAS carla-patchbay)
 
-# target_compile_definitions( carla-patchbay PRIVATE BUILDING_CARLA CARLA_BACKEND_NAMESPACE=CarlaPatchbayOBS
-# CARLA_MODULE_ID="carla_patchbay" CARLA_MODULE_NAME="Carla Patchbay" CARLA_PLUGIN_BUILD CARLA_PLUGIN_ONLY_BRIDGE
-# STATIC_PLUGIN_TARGET REAL_BUILD $<$<BOOL:${LIBMAGIC_FOUND}>:HAVE_LIBMAGIC> $<$<BOOL:${X11_FOUND}>:HAVE_X11>)
+target_compile_definitions(
+  carla-patchbay
+  PRIVATE BUILDING_CARLA
+          CARLA_BACKEND_NAMESPACE=CarlaPatchbayOBS
+          CARLA_MODULE_ID="carla_patchbay"
+          CARLA_MODULE_NAME="Carla Patchbay"
+          CARLA_PLUGIN_BUILD
+          CARLA_PLUGIN_ONLY_BRIDGE
+          STATIC_PLUGIN_TARGET
+          REAL_BUILD
+          $<$<BOOL:${LIBMAGIC_FOUND}>:HAVE_LIBMAGIC>
+          $<$<BOOL:${X11_FOUND}>:HAVE_X11>)
 
-# target_include_directories( carla-patchbay PRIVATE carla/source carla/source/backend carla/source/includes
-# carla/source/modules carla/source/utils ${LIBMAGIC_INCLUDE_DIRS} ${X11_INCLUDE_DIRS})
+target_compile_options(carla-patchbay PRIVATE -Wno-vla)
 
-# target_link_directories(carla-patchbay PRIVATE ${LIBMAGIC_LIBRARY_DIRS})
+target_include_directories(
+  carla-patchbay
+  PRIVATE carla/source
+          carla/source/backend
+          carla/source/includes
+          carla/source/modules
+          carla/source/utils
+          ${LIBMAGIC_INCLUDE_DIRS}
+          ${X11_INCLUDE_DIRS})
 
-# target_link_libraries( carla-patchbay PRIVATE # OBS::carla_jackbridge OBS::carla_rtmempool OBS::carla_water
-# OBS::libobs OBS::frontend-api Qt::Core Qt::Widgets ${LIBMAGIC_LIBRARIES} ${X11_LIBRARIES} )
+target_link_directories(carla-patchbay PRIVATE ${LIBMAGIC_LIBRARY_DIRS})
 
-# if(NOT OS_MACOS) target_link_options(carla-patchbay PRIVATE -Wl,--no-undefined) endif()
+target_link_libraries(
+  carla-patchbay
+  PRIVATE carla::jackbridge
+          carla::rtmempool
+          carla::water
+          OBS::libobs
+          OBS::frontend-api
+          Qt::Core
+          Qt::Widgets
+          ${LIBMAGIC_LIBRARIES}
+          ${X11_LIBRARIES})
 
-# target_sources( carla-patchbay PRIVATE carla.c carla-patchbay-wrapper.c common.c qtutils.cpp
-# carla/source/backend/engine/CarlaEngine.cpp carla/source/backend/engine/CarlaEngineClient.cpp
-# carla/source/backend/engine/CarlaEngineData.cpp carla/source/backend/engine/CarlaEngineGraph.cpp
-# carla/source/backend/engine/CarlaEngineInternal.cpp carla/source/backend/engine/CarlaEngineNative.cpp
-# carla/source/backend/engine/CarlaEnginePorts.cpp carla/source/backend/engine/CarlaEngineRunner.cpp
-# carla/source/backend/plugin/CarlaPlugin.cpp carla/source/backend/plugin/CarlaPluginBridge.cpp
-# carla/source/backend/plugin/CarlaPluginInternal.cpp )
+if(NOT OS_MACOS)
+  target_link_options(carla-patchbay PRIVATE -Wl,--no-undefined)
+endif()
 
-# set_target_properties(carla-patchbay PROPERTIES FOLDER "plugins/carla")
+target_sources(
+  carla-patchbay
+  PRIVATE carla.c
+          carla-patchbay-wrapper.c
+          common.c
+          qtutils.cpp
+          carla/source/backend/engine/CarlaEngine.cpp
+          carla/source/backend/engine/CarlaEngineClient.cpp
+          carla/source/backend/engine/CarlaEngineData.cpp
+          carla/source/backend/engine/CarlaEngineGraph.cpp
+          carla/source/backend/engine/CarlaEngineInternal.cpp
+          carla/source/backend/engine/CarlaEngineNative.cpp
+          carla/source/backend/engine/CarlaEnginePorts.cpp
+          carla/source/backend/engine/CarlaEngineRunner.cpp
+          carla/source/backend/plugin/CarlaPlugin.cpp
+          carla/source/backend/plugin/CarlaPluginBridge.cpp
+          carla/source/backend/plugin/CarlaPluginInternal.cpp)
 
-# setup_plugin_target(carla-patchbay)
+set_target_properties(carla-patchbay PROPERTIES FOLDER "plugins/carla")
+
+setup_plugin_target(carla-patchbay)
 
 # ######################################################################################################################
 
