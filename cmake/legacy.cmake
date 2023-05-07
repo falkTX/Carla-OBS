9b62a721c3b8b4c27d43108318d999919042474d
falkTX <falktx@falktx.com>
2023-05-07 14:54:00 +0200

    cleanup
    Signed-off-by: falkTX <falktx@falktx.com>

--- cmake/legacy.cmake
+++ cmake/legacy.cmake
@@ -75,11 +75,8 @@ target_link_libraries(
   carla-bridge
   PRIVATE carla::jackbridge
           carla::lilv
-          # carla::water
           OBS::libobs
-          # OBS::frontend-api
           Qt::Core
-          # Qt::Gui
           Qt::Widgets
           ${LIBMAGIC_LIBRARIES})
 
@@ -183,14 +180,3 @@ target_sources(
 set_target_properties(carla-patchbay PROPERTIES FOLDER "plugins/carla")
 
 setup_plugin_target(carla-patchbay)
-
-# ######################################################################################################################
-
-# if(_QT_VERSION EQUAL 6 AND OS_WINDOWS) set_target_properties(carla-bridge PROPERTIES AUTORCC_OPTIONS
-# "--format-version;1") endif()
-
-# ######################################################################################################################
-
-# setup_plugin_target(carla-bridge) setup_plugin_target(carla-patchbay)
-
-# ######################################################################################################################
